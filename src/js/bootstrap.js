d667e97487eb16e75161013db28ea2e873028e22
Colin Tester <github@z-omo.com>
2017-09-29 20:47:51 +0100

    Tidy config.

---
+++ src/js/bootstrap.js
@@ -0,0 +1,16 @@
+/**
+ * Bootstrap JavaScript file for website.com
+ *
+ * @package     Website
+ * @author      Colin Tester <office@z-omo.com>
+ * 
+ * This file forms part of the installed @package and should not be copied 
+ * and/or distributed without the written consent from the file's author.
+ *
+ * @copyright: Please see: <https://en.wikipedia.org/wiki/Berne_Convention>
+ *
+ * This file and its @package are under version control.
+ */
+require('../sass/bootstrap.sass');
+ 
+console.log('Bootstrap loaded.');
